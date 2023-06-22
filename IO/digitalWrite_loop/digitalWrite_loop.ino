d5256da8a0e34ddbce5fdc83b82b3796dfd2b8e3
Alex8695 <39283168+Alex8695@users.noreply.github.com>
2023-06-22 15:16:30 -0400

    Update digitalWrite_loop.ino
    1:  Added comments
    
    2:  Changed variables to constants: 
         •Conserved 2 byes of program storage space

--- IO/digitalWrite_loop/digitalWrite_loop.ino
+++ IO/digitalWrite_loop/digitalWrite_loop.ino
@@ -1,8 +1,8 @@
-
-int pins[]={2,3,4,5,6};
-int delayShort = 10;
-int delayLong = 20;
-int pinCount = 5;
+//Define Constants:
+const int pins[]={2,3,4,5,6}; //Array of pins being utilized
+const int delayShort = 10;    //Short delay in milliseconds
+const int delayLong = 20;     //Long delay in milliseconds
+const int pinCount = 5;       //Number of pins being utilized (size of pins)
 
 void setup() {
   // put your setup code here, to run once:
@@ -15,6 +15,8 @@ void setup() {
   Serial.print("==================");
   Serial.println();
 
+  //Initial setup of board
+  //Set pin mode to output
   for(int i=0; i<pinCount; i++){
       pinMode(pins[i],OUTPUT);
       }
@@ -23,12 +25,19 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
+  //Set all pins to low.  
+  //Apply a short delay before setting the next pin
+     
   for(int i=0; i<pinCount; i++){
       digitalWrite(pins[i],LOW);
       delay(delayShort);
-      }          
+      }        
+
+    //Apply a long delay before setting pins high
     delay(delayLong);
-  
+
+  //Set all pins to high
+  //Apply a short delay before setting the next pin
   for(int i=0; i<pinCount; i++){
       digitalWrite(pins[i],HIGH);
       delay(delayShort);
